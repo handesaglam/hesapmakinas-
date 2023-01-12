71facc534ea51ee89b40af8ea35313efe67186f2
Hande SAĞLAM <63313545+handesaglam@users.noreply.github.com>
2023-01-12 13:49:27 +0300

    update

--- hesapmakinası/ViewController.swift
+++ hesapmakinası/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+  
+    @IBOutlet weak var sayı1: UITextField!
+    
+    @IBOutlet weak var sayı2: UITextField!
+    
+    @IBOutlet weak var sonuc: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sonuc.text=""
+  
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hesapla(_ sender: Any) {
+        let say1=Int(sayı1.text!)!
+        let say2=Int(sayı2.text!)!
+        let not=say1+say2
+        
+        sonuc.text="\(not)"
+    }
+    
+    
+    @IBAction func sub(_ sender: Any) {
+        let say1=Int(sayı1.text!)!
+        let say2=Int(sayı2.text!)!
+        let not=say1-say2
+        
+        sonuc.text="\(not)"
+    }
+    
+    @IBAction func div(_ sender: Any) {
+        let say1=Int(sayı1.text!)!
+        let say2=Int(sayı2.text!)!
+        let not=say1/say2
+        
+        sonuc.text="\(not)"
+    }
+    
+    
+    @IBAction func add(_ sender: Any) {
+        let say1=Int(sayı1.text!)!
+        let say2=Int(sayı2.text!)!
+        let not=say1+say2
+        
+        sonuc.text="\(not)"
+        
+    }
+    
 }
 
